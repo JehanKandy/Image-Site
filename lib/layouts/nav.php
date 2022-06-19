90c38b999bb2dc73ae1f01dab87282c89c90dd25
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-19 17:53:31 +0530

    Update nav.php

--- lib/layouts/nav.php
+++ lib/layouts/nav.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="../../css/style.css">
 <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">&nbsp;&nbsp;Image Site</a>
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Wallpapers</a>
     <span class="navbar-text">
         <a href="lib/views/reg.php">New to site</a> &nbsp;&nbsp;&nbsp;   
         <a href="lib/views/login.php"><button class="login-btn-nav"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
