66763ced62b3261d44e5688781ece5bb0b25138f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-20 19:34:29 +0530

    Add files via upload

---
+++ lib/layouts/nav_login.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="../../css/style.css">
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">&nbsp;&nbsp;Wallpapers</a>
+    <span class="navbar-text">
+        <a href="../views/reg.php">New to site</a> &nbsp;&nbsp;&nbsp;   
+        <a href="../../index.php"><button class="login-btn-nav">  &nbsp;Back</button></a>&nbsp;&nbsp;&nbsp;&nbsp;
+    </span>
+</nav>
