552e609f7db401c5dbcc9185d7d9a41f95d4b704
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-06-20 20:56:04 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -6,7 +6,7 @@
         <div class="login-box">
             <div class="login-box-content">
                 <div class="content-title">
-                    Login Here
+                    <i class='fas fa-user-alt'>&nbsp;&nbsp;</i>Login Here
                     <hr class="hr">
                 </div>
                 <div class="content-body">
@@ -14,14 +14,15 @@
                         <p class="label">Username </p>
                         <input type="text" name="username" id="username" placeholder="Username" class="input-login">
                         <p class="label">Password </p>
-                        <input type="password" name="password" id="password" placeholder="Password" class="input-login"><br>
+                        <input type="password" name="password" id="pass" placeholder="Password" class="input-login">
                         <input type="submit" value="Login" name="login_user" class="login-btn">
                     </form>
                     <p class="no-account">Don't have an Account ? <a href="reg.php">Create One</a></p>
-                </div>
+                </div>               
             </div>
         </div>
     </div>
 </div>
 
+<script src="../../js/script.js"></script>
 <?php include_once("../layouts/footer_small.php"); ?>
